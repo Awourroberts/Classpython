72cca7827b4d6baea046daa3b9c56552cc9321b1
Awourrobberrt <sophierobberrt@gmail.com>
2021-06-07 22:03:55 +0300

    third commit

--- bank.py
+++ bank.py
@@ -8,6 +8,7 @@ class Bank_account :
         self.phone_number=phone_number
         self.loan=loan
         self.statement=[]
+        self.loan=0
 
     def show_balance(self):
         return f"Hello this is my {self.balance}"   
@@ -40,6 +41,7 @@ class Bank_account :
         return self.show_balance()    
 
     def borrow_loan(self,amount):
+   
         return f"congratulation you have borrowed {amount}"
     def repay_loan(self,amount):
         return f"You have  repaid {amount}"
@@ -49,9 +51,68 @@ class Bank_account :
                 amount= transacation["amount"]
                 narration=transacation["narration"]
                 time=transacation["time"]
-                date=time.strftime["%d/%m/%y"]
+                date=time.strftime("%d/%m/%y") 
                 print( f"{date}:{narration} {amount}")
         return
+    def borrow(self,amount):
+        if amount < 0:
+            return f"you have no outstanding loan { self.amount}"
+        elif self.loan>0:
+            return f"You have reach the limit borrowing loan {self.amount}"
+        elif amount<0.1 *self. balance:
+            return f"you did qualify for the loan of ksh{amount}"
+        else:
+            loan=amount*1.05
+            self.loan=loan 
+            self.balance +=amount
+            return f"your  outstanding loan is{self.loan}"
+    def repay_loan(self,amount):
+        if amount < 0:
+            return f"you cannot repay with that "
+        elif amount > self.loan:
+            return f"you repay the loan with decrease {self.amount}"
+        
+        elif amount < self.loan:
+            self.loan-=amount
+            return f"you have successful repaid {self.loan}"
+        else:
+            diff=amount-self .loan
+            self ._loan=0 
+            self .deposit(diff)
+            now=datetime.now()
+            transaction={
+
+            }
+            self.statement.append(transaction)
+            return self.show_balance()
+
+    def show_statement(self):
+        for transacation in self.statement:
+                amount= transacation["amount"]
+                narration=transacation["narration"]
+                time=transacation["time"]
+                date=time.strftime("%d/%m/%y") 
+                print( f"{date}:{narration} {amount}")
+
+            
+
+
+
+               
+
+
+
+
+
+
+    
+        
+
+
+            
+                
+
+
         
 
 
